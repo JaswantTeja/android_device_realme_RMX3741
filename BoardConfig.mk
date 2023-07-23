66df8e60e24855ec5a8590211cfad7079a845780
bengris32 <bengris32@protonmail.ch>
2023-07-23 13:40:02 +0530

    RMX3741: Initial skeleton
    Signed-off-by: JaswantTeja <sunnyteja201@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/realme/RMX3741
+
+# Inherit the proprietary files
+include vendor/realme/RMX3741/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,9 @@
+# Android Device tree for Realme 11 Pro Plus
+
+```
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
